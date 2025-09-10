fb9da82b1b490f70ce41ab24666e2aec0b2d95e6
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-11 00:03:49 +0530

    feat(auth): add authentication middleware and extend request type with user data
    - Extend Request type with AuthRequest interface to include optional user property.
    - Implement authenticate middleware to validate JWT token and attach user data to the request.
    - Middleware checks for 'Bearer' token in Authorization header, verifies token, and populates req.user.

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import type { Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt.util.js";
+import type { AuthRequest } from "../types/auth-request.type.js";
+import { UnauthorizedError } from "../utils/http-error.util.js";
+import type { UserResponseDTO } from "../dto/user-response.dto.js";
+
+// /src/middlewares/auth.middleware.ts
+
+export function authenticate(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthorizedError()
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    throw new UnauthorizedError();
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded as UserResponseDTO;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
