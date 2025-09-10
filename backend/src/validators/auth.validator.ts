4b69837e3862dd84005e5a90cf8c0bf81006ee6f
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-10 23:42:59 +0530

    feat(auth): add validation schemas and DTOs for user registration and login

---
+++ backend/src/validators/auth.validator.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+// /src/validators/auth.validator.ts
+
+// Schema for Register User
+export const RegisterUserSchema = z.object({
+  email: z.email().min(1, "Email is required"),
+  password: z.string().min(8, "Password should have at least 8 characters"),
+});
+
+// Schema for Login User
+export const LoginUserSchema = z.object({
+  email: z.email().min(1, "Email is required"),
+  password: z.string().min(8, "Password should have at least 8 characters"),
+});
+
