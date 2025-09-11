79e8698cb970db3d3c121d78c0f10cd5b10eaaca
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-11 19:36:09 +0530

    feat(routes): add metrics route and update ad-accounts path under app router

--- backend/src/routes/app.routes.ts
+++ backend/src/routes/app.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authRouter } from "./auth.routes.js";
 import { adAccountsRouter } from "./ad-account.routes.js";
+import { metricsRouter } from "./metrics.route.js";
 
 // /src/routes/app.routes.ts
 
 export const appRouter = Router();
 
 appRouter.use('/auth', authRouter);
-appRouter.use('/api/ad-accounts', adAccountsRouter);
+appRouter.use('/ad-accounts', adAccountsRouter);
+appRouter.use('/metrics', metricsRouter);
 
