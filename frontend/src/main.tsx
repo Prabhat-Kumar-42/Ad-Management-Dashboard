f273189eb7377b0941663f30b95f80412978f75d
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-20 20:32:17 +0530

    feat: wrap App with ThemeProvider, QueryClientProvider, and BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './contexts/ThemeContext/ThemeContext.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+// src/main.tsx
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
